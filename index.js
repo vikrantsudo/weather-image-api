dfc2b0c48f1ea83424161306068c337439cf5082
Boltista <vikrantwnn@gmail.com>
2026-03-12 14:01:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.get("/state", async (req, res) => {
+
+const state = req.query.name || "London";
+
+try {
+
+const response = await axios.get(`https://wttr.in/${state}?format=j1`);
+const data = response.data;
+
+const weather = data.current_condition[0];
+const area = data.nearest_area[0];
+
+const city = area.areaName[0].value;
+const country = area.country[0].value;
+
+const tempC = weather.temp_C;
+const tempF = weather.temp_F;
+const wind = weather.windspeedKmph;
+const humidity = weather.humidity;
+const status = weather.weatherDesc[0].value;
+
+const svg = `
+<svg width="900" height="320" xmlns="http://www.w3.org/2000/svg">
+
+<rect x="5" y="5" width="890" height="310" fill="white" stroke="black" stroke-width="6"/>
+
+<text x="70" y="290" font-size="16">weather status for ${city} in ${country}</text>
+
+<text x="300" y="80" font-size="22">${status}</text>
+
+<text x="300" y="150" font-size="20">Temp: ${tempC}°C</text>
+<text x="300" y="200" font-size="20">Temp: ${tempF}°F</text>
+
+<text x="620" y="120" font-size="20">Wind: ${wind} km/h</text>
+<text x="620" y="200" font-size="20">Humidity: ${humidity}%</text>
+
+<circle cx="150" cy="120" r="50" fill="yellow" stroke="black" stroke-width="3"/>
+
+</svg>
+`;
+
+res.setHeader("Content-Type","image/svg+xml");
+res.send(svg);
+
+} catch(err) {
+
+res.send("Weather error");
+
+}
+
+});
+
+app.listen(3000, () => {
+console.log("Weather API running");
+});
